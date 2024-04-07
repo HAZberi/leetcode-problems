52dcf0ca07c5d0830769257a0dd530af9ffe91e1
Hassaan Zuberi <mhazuberi@gmail.com>
2024-04-06 21:51:45 -0600

    coded two sum complete

--- 1-Two-sum/1-two-sum.py
+++ 1-Two-sum/1-two-sum.py
@@ -5,4 +5,17 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-    
+        prevMap = {} #{value: index} of visited elements in the nums array
+
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[nums[i]] = i
+
+#TestCases
+mySolution = Solution()
+
+print("Test Case 1:", mySolution.twoSum([2,7,11,15], 9))
+print("Test Case 2:", mySolution.twoSum([3,2,4], 6))
+print("Test Case 3:", mySolution.twoSum([3,3], 6))
