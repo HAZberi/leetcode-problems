7d5072e5299a306588795080e0d04669353a020c
Hassaan Zuberi <mhazuberi@gmail.com>
2024-04-06 18:46:42 -0600

    coded solution with test cases.

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,3 +1,22 @@
-#Notes
-#1. Brute force solution requires two loops and the time complexity in that case is O(n)
-#2. 
+class Solution(object):
+    def containsDuplicate(self, nums) -> bool:
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        hashset = set()
+
+        for i in range(len(nums)):
+            if nums[i] in hashset:
+                return True
+            hashset.add(nums[i])
+        
+        return False
+    
+#Test Cases
+
+mySolution = Solution()
+
+print("Test Case 1:", mySolution.containsDuplicate([1,2,3,1]))
+print("Test Case 2:", mySolution.containsDuplicate([1,2,3,4]))
+print("Test Case 3:", mySolution.containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
