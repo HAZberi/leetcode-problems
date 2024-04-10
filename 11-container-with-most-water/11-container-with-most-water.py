7af22ed373827a29c955dffffeaca8bcf8245749
Hassaan Zuberi <mhazuberi@gmail.com>
2024-04-10 09:08:35 -0600

    coded 11 solution with test cases

---
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        res = 0
+
+        l, r = 0, len(height) - 1
+
+        while l < r:
+            area = (r - l) * min(height[l], height[r])
+            res = max(area, res)
+
+            if height[l] < height [r]:
+                l += 1
+            else:
+                r -= 1
+        return res
+
+#Test Cases
+
+mySolution = Solution()
+
+print("Test Case 1:", mySolution.maxArea([1,8,6,2,5,4,8,3,7]))
+print("Test Case 1:", mySolution.maxArea([1,1]))
