9059a0e946dd7dbe3418b2553185e97b26a57fcb
Hassaan Zuberi <mhazuberi@gmail.com>
2024-04-26 13:17:58 -0600

    coded solution 567 optimized

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -32,10 +32,52 @@ class Solution: #O(26)
         return False
 
 
+class OptimizedSolution: #O(n)
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        
+        countS1 = [0] * 26
+        countS2 = [0] * 26
+
+        for i in range(len(s1)):
+            countS1[ord(s1[i]) - ord('a')] += 1
+            countS2[ord(s2[i]) - ord('a')] += 1
+
+        matches = 0
+        for i in range(26):
+            matches += (1 if countS1[i] == countS2[i] else 0)
+
+        L = 0
+        for R in range(len(s1), len(s2)):
+            if matches == 26: return True
+
+            index = ord(s2[R]) - ord('a')
+            countS2[index] += 1
+            if countS1[index] == countS2[index]:
+                matches += 1
+            else:
+                matches -= 1
+
+            index = ord(s2[L]) - ord('a')
+            countS2[index] -= 1
+            if countS1[index] == countS2[index]:
+                matches += 1
+            else:
+                matches -= 1
+        
+        return matches == 26
+
+
 #Test Case
 mySolution = Solution()
+myOptimizedSolution = OptimizedSolution()
 
 print(mySolution.checkInclusion("adc", "dcda"))
 print(mySolution.checkInclusion("ab", "eidboaio"))
 print(mySolution.checkInclusion("abc", "bcaa"))
 print(mySolution.checkInclusion("abb", "bbaa"))
+print(myOptimizedSolution.checkInclusion("adc", "dcda"))
+print(myOptimizedSolution.checkInclusion("ab", "eidboaio"))
+print(myOptimizedSolution.checkInclusion("abc", "bcaa"))
+print(myOptimizedSolution.checkInclusion("abb", "bbaa"))
