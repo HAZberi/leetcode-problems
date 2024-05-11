a581b7039048edb4e7e258bedaf6b8baae00cf20
Hassaan Zuberi <mhazuberi@gmail.com>
2024-05-11 00:15:15 -0600

    updated solution

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
@@ -23,12 +23,6 @@ class Solution:
 
         dfs(root)
         return diameter[0]
-    
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
-        
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
 
 def insert(root: TreeNode, val:int):
     
