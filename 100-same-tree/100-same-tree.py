c27424884c122988bcd1380a43def20a8909cd0b
Hassaan Zuberi <mhazuberi@gmail.com>
2024-05-12 13:50:20 -0600

    coded solution

---
+++ 100-same-tree/100-same-tree.py
@@ -0,0 +1,70 @@
+from typing import List, Optional
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right 
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        
+        if not p or not q or p.val != q.val:
+            return False
+        
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+def insert(root: TreeNode, val:int, visited):
+    if not root:
+        root = TreeNode(val) if val else None
+        return 
+    
+    queue = deque()
+    queue.append(root)
+    while len(queue) > 0:
+        curr = queue.popleft()
+        if not curr.left and curr not in visited:
+            curr.left = TreeNode(val) if val else None
+            if not curr.left:
+                visited[curr] = [-1, -1]
+                visited[curr][0] = 0
+            break
+        elif not curr.left and curr in visited and visited[curr][0] != 0:
+            curr.left = TreeNode(val) if val else None
+            if not curr.left:
+                visited[curr][0] = 0 
+            break
+        else:
+            queue.append(curr.left)
+        if not curr.right and curr not in visited:
+            curr.right = TreeNode(val) if val else None
+            if not curr.right:
+                visited[curr] = [-1, -1]
+                visited[curr][1] = 1
+            break
+        elif not curr.right and curr in visited and visited[curr][1] != 1:
+            curr.right = TreeNode(val) if val else None
+            if not curr.right:
+                visited[curr][1] = 1
+            break
+        else:
+            queue.append(curr.right)
+        
+def populateTree(list: List) -> Optional[TreeNode]:
+    if len(list) < 1:
+        return None
+    
+    root = TreeNode(list[0])
+    visited = {} #CurrentNode: [0 for left, 1 for right]
+    for i in range(1, len(list)):
+        insert(root, list[i], visited)
+    
+    return root
+
+#Test Cases:
+mySolution = Solution()
+print("Test Case 1: ", mySolution.isSameTree(populateTree([1,2,3]), populateTree([1,2,3])))
+print("Test Case 2: ", mySolution.isSameTree(populateTree([1,2]), populateTree([1,None,2])))
+print("Test Case 3: ", mySolution.isSameTree(populateTree([1,2,1]), populateTree([1,1,2])))
