2c839558d9f35d97ddc97c69329268672eb4c028
Hassaan Zuberi <mhazuberi@gmail.com>
2024-05-13 17:16:41 -0600

    coded 102

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.md
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.md
@@ -9,3 +9,4 @@
 7. The we start an inner for loop, that runs till the current length of queue. Inside this loop we first pop left a node. Append it to the level list. Then we check if this node has a left child. If yes, we append the left child to the queue. Then we check if this node has a right child. If yes we append the right child to the queue.
 8. Once the for loop exits. We append the level list to our result. An then move on to the next iteration of the outer loop. And continue the same process.
 9. At the end return the result.
+10. The time complexity of this solution is O(n) and the space complexity is also O(n) since we are using a queue.
---
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
@@ -0,0 +1,84 @@
+from collections import deque
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        result = []
+        if not root:
+            return result
+        
+        queue = deque()
+        queue.append(root)
+
+        while queue:
+            level = []
+            for i in range(len(queue)):
+                curr = queue.popleft()
+                level.append(curr.val)
+                if curr.left:
+                    queue.append(curr.left)
+                if curr.right:
+                    queue.append(curr.right)
+            result.append(level)
+        
+        return result
+
+
+def insert(root: TreeNode, val:int, visited):
+    if not root:
+        root = TreeNode(val) if val else None
+        return 
+    
+    queue = deque()
+    queue.append(root)
+    while len(queue) > 0:
+        curr = queue.popleft()
+        if not curr.left and curr not in visited:
+            curr.left = TreeNode(val) if val else None
+            if not curr.left:
+                visited[curr] = [-1, -1]
+                visited[curr][0] = 0
+            break
+        elif not curr.left and curr in visited and visited[curr][0] != 0:
+            curr.left = TreeNode(val) if val else None
+            if not curr.left:
+                visited[curr][0] = 0 
+            break
+        else:
+            queue.append(curr.left)
+        if not curr.right and curr not in visited:
+            curr.right = TreeNode(val) if val else None
+            if not curr.right:
+                visited[curr] = [-1, -1]
+                visited[curr][1] = 1
+            break
+        elif not curr.right and curr in visited and visited[curr][1] != 1:
+            curr.right = TreeNode(val) if val else None
+            if not curr.right:
+                visited[curr][1] = 1
+            break
+        else:
+            queue.append(curr.right)
+        
+def populateTree(list: List) -> Optional[TreeNode]:
+    if len(list) < 1:
+        return None
+    
+    root = TreeNode(list[0])
+    visited = {} #CurrentNode: [0 for left, 1 for right]
+    for i in range(1, len(list)):
+        insert(root, list[i], visited)
+    
+    return root
+
+#Test Cases:
+mySolution = Solution()
+print("Test Case 1: ", mySolution.levelOrder(populateTree([3,9,20,None,None,15,7])))
+print("Test Case 2: ", mySolution.levelOrder(populateTree([1])))
+print("Test Case 3: ", mySolution.levelOrder(populateTree([])))
