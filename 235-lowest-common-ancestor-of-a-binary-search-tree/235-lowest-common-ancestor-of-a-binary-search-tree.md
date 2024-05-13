0d67751fda858fb139d6579c4566075c33843f30
Hassaan Zuberi <mhazuberi@gmail.com>
2024-05-12 23:58:04 -0600

    coded 235 complete

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.md
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.md
@@ -5,3 +5,4 @@
 3. There is one more case, if one of the node's value is equal to the parent/curr node. In that case we will also return this parent/curr node.
 4. So basically if both the node values is greater than the current or parent node value. We move to the right of the tree. By setting our current to right of current. If both the node values is smaller than the current of parent node value then we move to the left of the tree. By setting our current to left of current.
 5. In all other conditions, we have reached the lowest common anscestor, we will simply return current/parent.
+6. Since we are going through each level and hit only one node at each level. The time complexity of this algortihm is O(logn) and space is constant O(1)
---
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -0,0 +1,76 @@
+from collections import deque
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        curr = root
+
+        while curr:
+            if p.val < curr.val and q.val < curr.val:
+                curr = curr.left
+            elif p.val > curr.val and q.val > curr.val:
+                curr = curr.right
+            else:
+                return curr
+
+
+def insert(root: TreeNode, val:int, visited):
+    if not root:
+        root = TreeNode(val) if val or val==0 else None
+        return 
+    
+    queue = deque()
+    queue.append(root)
+    while len(queue) > 0:
+        curr = queue.popleft()
+        if not curr.left and curr not in visited:
+            curr.left = TreeNode(val) if val or val==0 else None
+            if not curr.left:
+                visited[curr] = [-1, -1]
+                visited[curr][0] = 0
+            break
+        elif not curr.left and curr in visited and visited[curr][0] != 0:
+            curr.left = TreeNode(val) if val or val==0 else None
+            if not curr.left:
+                visited[curr][0] = 0 
+            break
+        else:
+            queue.append(curr.left)
+
+        if not curr.right and curr not in visited:
+            curr.right = TreeNode(val) if val or val==0 else None
+            if not curr.right:
+                visited[curr] = [-1, -1]
+                visited[curr][1] = 1
+            break
+        elif not curr.right and curr in visited and visited[curr][1] != 1:
+            curr.right = TreeNode(val) if val or val==0 else None
+            if not curr.right:
+                visited[curr][1] = 1
+            break
+        else:
+            queue.append(curr.right)
+        
+def populateTree(list: List) -> Optional[TreeNode]:
+    if len(list) < 1:
+        return None
+    
+    root = TreeNode(list[0])
+    visited = {} #CurrentNode: [0 for left, 1 for right]
+    for i in range(1, len(list)):
+        insert(root, list[i], visited)
+    
+    return root
+
+#Test Cases:
+mySolution = Solution()
+print("Test Case 1: ", mySolution.lowestCommonAncestor(populateTree([6,2,8,0,4,7,9,None,None,3,5]), TreeNode(2), TreeNode(8)).val)
+print("Test Case 2: ", mySolution.lowestCommonAncestor(populateTree([6,2,8,0,4,7,9,None,None,3,5]), TreeNode(2), TreeNode(4)).val)
+print("Test Case 3: ", mySolution.lowestCommonAncestor(populateTree([2,1]), TreeNode(2), TreeNode(1)).val)
