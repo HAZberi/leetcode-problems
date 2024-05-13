45d8741fa57799f9f1d1489c07380d767b50ca03
Hassaan Zuberi <mhazuberi@gmail.com>
2024-05-12 20:36:58 -0600

    coded 572

---
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.py
@@ -0,0 +1,80 @@
+from collections import deque
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not subRoot: #Order is important here
+            return True
+        if not root:
+            return False
+        
+        if self.isSameTree(root, subRoot):
+            return True
+        
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q or p.val != q.val:
+            return False
+        
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+    
+def insert(root: TreeNode, val:int, visited):
+    if not root:
+        root = TreeNode(val) if val or val==0 else None
+        return 
+    
+    queue = deque()
+    queue.append(root)
+    while len(queue) > 0:
+        curr = queue.popleft()
+        if not curr.left and curr not in visited:
+            curr.left = TreeNode(val) if val or val==0 else None
+            if not curr.left:
+                visited[curr] = [-1, -1]
+                visited[curr][0] = 0
+            break
+        elif not curr.left and curr in visited and visited[curr][0] != 0:
+            curr.left = TreeNode(val) if val or val==0 else None
+            if not curr.left:
+                visited[curr][0] = 0 
+            break
+        else:
+            queue.append(curr.left)
+
+        if not curr.right and curr not in visited:
+            curr.right = TreeNode(val) if val or val==0 else None
+            if not curr.right:
+                visited[curr] = [-1, -1]
+                visited[curr][1] = 1
+            break
+        elif not curr.right and curr in visited and visited[curr][1] != 1:
+            curr.right = TreeNode(val) if val or val==0 else None
+            if not curr.right:
+                visited[curr][1] = 1
+            break
+        else:
+            queue.append(curr.right)
+        
+def populateTree(list: List) -> Optional[TreeNode]:
+    if len(list) < 1:
+        return None
+    
+    root = TreeNode(list[0])
+    visited = {} #CurrentNode: [0 for left, 1 for right]
+    for i in range(1, len(list)):
+        insert(root, list[i], visited)
+    
+    return root
+
+#Test Cases:
+mySolution = Solution()
+print("Test Case 1: ", mySolution.isSubtree(populateTree([3,4,5,1,2]), populateTree([4,1,2])))
+print("Test Case 2: ", mySolution.isSubtree(populateTree([3,4,5,1,2,None,None,None,None,0]), populateTree([4,1,2])))
