9cf9733a8865fc0bf898f5ec8223092bb8358fd3
Hassaan Zuberi <mhazuberi@gmail.com>
2024-05-13 22:59:49 -0600

    updated test code 104

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
@@ -49,35 +49,56 @@ class IterativeDFSSolution:
         
         return res
 
-def insert(root: TreeNode, val:int):
+def insert(root: TreeNode, val:int, visited):
+    if not root:
+        root = TreeNode(val) if val else None
+        return 
+    
     queue = deque()
     queue.append(root)
-
     while len(queue) > 0:
         curr = queue.popleft()
-        if not curr.left:
+        if not curr.left and curr not in visited:
             curr.left = TreeNode(val) if val else None
+            if not curr.left:
+                visited[curr] = [-1, -1]
+                visited[curr][0] = 0
             break
+        elif not curr.left and curr in visited and visited[curr][0] != 0:
+            curr.left = TreeNode(val) if val else None
+            if not curr.left:
+                visited[curr][0] = 0 
+            break
+        elif not curr.left and visited[curr][0] == 0:
+            visited[curr][0] = 0
         else:
             queue.append(curr.left)
-
-        if not curr.right:
+        if not curr.right and curr not in visited:
             curr.right = TreeNode(val) if val else None
+            if not curr.right:
+                visited[curr] = [-1, -1]
+                visited[curr][1] = 1
             break
+        elif not curr.right and curr in visited and visited[curr][1] != 1:
+            curr.right = TreeNode(val) if val else None
+            if not curr.right:
+                visited[curr][1] = 1
+            break
+        elif not curr.right and visited[curr][1] == 1:
+            visited[curr][1] = 1
         else:
             queue.append(curr.right)
-
-
+        
 def populateTree(list: List) -> Optional[TreeNode]:
     if len(list) < 1:
         return None
     
     root = TreeNode(list[0])
+    visited = {} #CurrentNode: [0 for left, 1 for right]
     for i in range(1, len(list)):
-        insert(root, list[i])
+        insert(root, list[i], visited)
     
     return root
-
 #Test Cases
 myRDFS = RecursiveDFSSolution()
 myBFS = BFSSolution()
