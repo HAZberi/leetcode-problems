15941844b1a1f945ca14168ed07ba5a19a5fe238
Hassaan Zuberi <mhazuberi@gmail.com>
2024-05-15 19:46:39 -0600

    coded solution 98

---
+++ binary-search/98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -0,0 +1,77 @@
+from collections import deque
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def dfs(root, left, right):
+            if not root:
+                return True
+            
+            if not (root.val > left and root.val < right):
+                return False
+            
+            return (self.isValidBST(root.left, left, root.val) and self.isValidBST(root.right, root.val, right))
+        
+        return dfs(root, float("-inf"), float("inf"))
+    
+def insert(root: TreeNode, val:int, visited):
+    if not root:
+        root = TreeNode(val) if val else None
+        return 
+    
+    queue = deque()
+    queue.append(root)
+    while len(queue) > 0:
+        curr = queue.popleft()
+        if not curr.left and curr not in visited:
+            curr.left = TreeNode(val) if val else None
+            if not curr.left:
+                visited[curr] = [-1, -1]
+                visited[curr][0] = 0
+            break
+        elif not curr.left and curr in visited and visited[curr][0] != 0:
+            curr.left = TreeNode(val) if val else None
+            if not curr.left:
+                visited[curr][0] = 0 
+            break
+        elif not curr.left and visited[curr][0] == 0:
+            visited[curr][0] = 0
+        else:
+            queue.append(curr.left)
+        if not curr.right and curr not in visited:
+            curr.right = TreeNode(val) if val else None
+            if not curr.right:
+                visited[curr] = [-1, -1]
+                visited[curr][1] = 1
+            break
+        elif not curr.right and curr in visited and visited[curr][1] != 1:
+            curr.right = TreeNode(val) if val else None
+            if not curr.right:
+                visited[curr][1] = 1
+            break
+        elif not curr.right and visited[curr][1] == 1:
+            visited[curr][1] = 1
+        else:
+            queue.append(curr.right)
+        
+def populateTree(list: List) -> Optional[TreeNode]:
+    if len(list) < 1:
+        return None
+    
+    root = TreeNode(list[0])
+    visited = {} #CurrentNode: [0 for left, 1 for right]
+    for i in range(1, len(list)):
+        insert(root, list[i], visited)
+    
+    return root
+
+#Test Cases:
+mySolution = Solution()
+print("Test Case 1: ", mySolution.isValidBST(populateTree([2,1,3])))
+print("Test Case 2: ", mySolution.isValidBST(populateTree([5,1,4,None,None,3,6])))
