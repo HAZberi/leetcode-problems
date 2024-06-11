aed8df3d66b744a7eb0acbf282a55ccebc6a76ea
Hassaan Zuberi <mhazuberi@gmail.com>
2024-06-11 20:18:41 +0300

    coded solution 295

---
+++ heap-priority-queue/295-find-median-from-data-stream/295-find-median-from-data-stream.py
@@ -0,0 +1,39 @@
+import heapq
+
+
+class MedianFinder:
+
+    def __init__(self):
+        self.small = []
+        self.large = []
+        
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.small, -1 * num)
+
+        if self.small and self.large and (-1 * self.small[0]) > self.large[0]:
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+        
+        if len(self.small) > len(self.large) + 1:
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+        
+        if len(self.large) > len(self.small) + 1:
+            val = heapq.heappop(self.large)
+            heapq.heappush(self.small, -1 * val)
+
+    def findMedian(self) -> float:
+        if len(self.small) > len(self.large):
+            return -1 * self.small[0]
+        if len(self.large) > len(self.small):
+            return self.large[0]
+        
+        return (-1 * self.small[0] + self.large[0]) / 2
+
+#Test Cases
+mySolution = MedianFinder()
+print("Add 1: ", mySolution.addNum(1))
+print("Add 2: ", mySolution.addNum(2))
+print("Median: ", mySolution.findMedian())
+print("Add 2: ", mySolution.addNum(3))
+print("Median: ", mySolution.findMedian())
