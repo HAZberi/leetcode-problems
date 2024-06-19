30fac00597d2041f18cc5f08d2df7dabb6aa5daa
Hassaan Zuberi <mhazuberi@gmail.com>
2024-06-19 15:48:22 +0300

    coded solution 200

---
+++ graphs/200-number-of-islands/200-number-of-islands.py
@@ -0,0 +1,51 @@
+from collections import deque
+from typing import List
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        
+        rows, cols = len(grid), len(grid[0])
+        visited = set()
+        islands = 0
+
+        def bfs(r, c):
+            queue = deque()
+            visited.add((r, c))
+            queue.append((r, c))
+
+            neighbours = [[0,1],[0,-1],[1,0],[-1,0]]
+
+            while queue:
+                row, col = queue.popleft()
+                
+                for dr, dc in neighbours:
+                    r, c = row + dr, col + dc
+                    if r in range(rows) and c in range(cols) and grid[r][c] == "1" and (r, c) not in visited:
+                        queue.append((r, c))
+                        visited.add((r, c))
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r,c) not in visited:
+                    bfs(r, c)
+                    islands += 1
+        
+        return islands
+
+#Test Cases
+mySolution = Solution()
+print("Test Case 1: ", mySolution.numIslands([
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]))
+print("Test Case 2: ", mySolution.numIslands([
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]))
