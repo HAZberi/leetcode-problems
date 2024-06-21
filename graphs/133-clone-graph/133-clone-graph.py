93b995e45607d2c9fda42c3974b794177dc3e3e6
Hassaan Zuberi <mhazuberi@gmail.com>
2024-06-22 01:24:42 +0300

    coded solution for 133 with test cases

--- graphs/133-clone-graph/133-clone-graph.py
+++ graphs/133-clone-graph/133-clone-graph.py
@@ -5,7 +5,7 @@ class Node:
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 
-from typing import Optional
+from typing import List, Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         oldToNew = {}
@@ -24,4 +24,34 @@ class Solution:
         return dfs(node) if node else None
 
 #Test Cases
+def graphSize(adj_list: List) -> int:
+    return len(adj_list)
+
+def make_graph(adj_list: List) -> Node:
+    if not adj_list:
+        return None
+    nodes = [Node(i + 1) for i in range(len(adj_list))]
+    for i, neighbors in enumerate(adj_list):
+        nodes[i].neighbors = [nodes[j-1] for j in neighbors]
+    return nodes[0]
+
+def make_list(node: Node, size: int) -> List:
+    if not Node:
+        return []
+    list = [0] * size
+    while size > 0 and node:
+        if list[node.val - 1] != 0:
+            node = node.neighbors.pop() if node.neighbors else None
+            continue
+
+        list[node.val - 1] = [neighbour.val for neighbour in node.neighbors] if node.neighbors else []
+        node = node.neighbors.pop() if node.neighbors else None 
+        size -= 1
+    
+    return list
+            
+
 mySolution = Solution()
+print("Test Case 1: ", make_list(mySolution.cloneGraph(make_graph([[2,4],[1,3],[2,4],[1,3]])), graphSize([[2,4],[1,3],[2,4],[1,3]])))
+print("Test Case 2: ", make_list(mySolution.cloneGraph(make_graph([[]])), graphSize([[]])))
+print("Test Case 3: ", make_list(mySolution.cloneGraph(make_graph([])), graphSize([])))
