c2538b46cad000ce8a580c0ee57cafe0dcccbc24
Hassaan Zuberi <mhazuberi@gmail.com>
2024-07-25 17:29:50 -0600

    coded solution 97

--- 2-dp/97-interleaving-string/97-interleaving-string.py
+++ 2-dp/97-interleaving-string/97-interleaving-string.py
@@ -26,6 +26,28 @@ print("Test Case 1: ", my_topDown_solution.isInterleave("aabcc", "dbbca", "aadbb
 print("Test Case 2: ", my_topDown_solution.isInterleave("aabcc", "dbbca", "aadbbbaccc"))
 print("Test Case 3: ", my_topDown_solution.isInterleave("", "", ""))
 
+class BottomUpSolution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        dp = [[False for _ in range(len(s2) + 1)] for _ in range(len(s1) + 1)]
+        dp[len(s1)][len(s2)] = True
+
+        for i in range(len(s1), -1, -1):
+            for j in range(len(s2), -1, -1):
+                if i < len(s1) and s1[i] == s3[i + j] and dp[i + 1][j]:
+                    dp[i][j] = True
+                if j < len(s2) and s2[j] == s3[i + j] and dp[i][j + 1]:
+                    dp[i][j] = True
+        return dp[0][0]
+
+
+#Test Case
+my_bottomUp_solution = BottomUpSolution()
+print("Test Case 1: ", my_bottomUp_solution.isInterleave("aabcc", "dbbca", "aadbbcbcac"))
+print("Test Case 2: ", my_bottomUp_solution.isInterleave("aabcc", "dbbca", "aadbbbaccc"))
+print("Test Case 3: ", my_bottomUp_solution.isInterleave("", "", ""))
+
+
+
 
 
 
