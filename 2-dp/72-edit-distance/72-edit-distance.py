797578a7e4e99a28e6141b54e5901b956a4acbd7
Hassaan Zuberi <mhazuberi@gmail.com>
2024-07-25 22:48:53 -0600

    coded solution 72

--- 2-dp/72-edit-distance/72-edit-distance.py
+++ 2-dp/72-edit-distance/72-edit-distance.py
@@ -30,5 +30,27 @@ print("Test Case 1: ", my_topDown_solution.minDistance("horse", "ros"))
 print("Test Case 2: ", my_topDown_solution.minDistance("intention", "execution"))
 
 
+class BottomUpSolution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        dp = [[-1 for _ in range(len(word2) + 1)] for _ in range(len(word1) + 1)]
 
+        for i in range(len(word1) + 1):
+            dp[i][len(word2)] = len(word1) - i
+        for j in range(len(word2) + 1):
+            dp[len(word1)][j] = len(word2) - j
+        
+        print(dp)
+
+        for i in range(len(word1) - 1, -1, -1):
+            for j in range(len(word2) - 1, -1, -1):
+                if word1[i] == word2[j]:
+                    dp[i][j] = dp[i + 1][j + 1]
+                else:
+                    dp[i][j] = 1 + min(dp[i][j + 1], dp[i + 1][j], dp[i + 1][j + 1])
+        return dp[0][0]
+
+#Test Case
+my_bottomUp_solution = BottomUpSolution()
+print("Test Case 1: ", my_bottomUp_solution.minDistance("horse", "ros"))
+print("Test Case 2: ", my_bottomUp_solution.minDistance("intention", "execution"))
 
