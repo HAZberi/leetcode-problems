01891a33c0aa62675e728d72823a1c41bd598d26
Hassaan Zuberi <mhazuberi@gmail.com>
2025-03-02 04:08:55 -0700

    sound pattern parsed

--- neo-practice/blueprint-coding-challenge-2.py
+++ neo-practice/blueprint-coding-challenge-2.py
@@ -6,4 +6,34 @@ def fetch_data():
     result = json.loads(response.text)
     return result
 
-print(fetch_data())
+def sound_pattern(sound):
+    allowed_pattern = [] #list of sets
+
+    i = 0
+    open = False
+    while i < len(sound):
+        if sound[i] == "(":
+            open = True
+            i += 1
+        elif open == True:
+            group = set()
+            while sound[i] != ")":
+                group.add(sound[i])
+                i += 1
+            allowed_pattern.append(group)
+            open = False
+            i += 1
+        else:
+            allowed_pattern.append(set([sound[i]]))
+            i += 1
+    
+    return allowed_pattern
+
+print(sound_pattern("(dc)(ba)"))
+
+        
+        
+
+
+
+
